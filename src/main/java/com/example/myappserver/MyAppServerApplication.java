e9d14c32a48cc543c9bd6c89ab692dc76d745fff
chengzongxin <8876142@qq.com>
2024-11-09 00:04:53 +0800

    run

--- src/main/java/com/example/myappserver/MyAppServerApplication.java
+++ src/main/java/com/example/myappserver/MyAppServerApplication.java
@@ -2,8 +2,10 @@ package com.example.myappserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.example.myappserver.mapper")
 public class MyAppServerApplication {
 
 	public static void main(String[] args) {
