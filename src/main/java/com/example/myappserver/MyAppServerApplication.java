71acdcd2087460cb5fb284092b28bf396afe5d11
chengzongxin <8876142@qq.com>
2024-11-08 23:17:24 +0800

    first init

---
+++ src/main/java/com/example/myappserver/MyAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myappserver/MyAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
