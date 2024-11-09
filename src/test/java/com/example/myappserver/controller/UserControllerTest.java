fe03166b7dc7138b6b7f13ab9f2922a795a9e59c
chengzongxin <8876142@qq.com>
2024-11-10 00:02:38 +0800

    deploy fix

--- src/test/java/com/example/myappserver/controller/UserControllerTest.java
+++ src/test/java/com/example/myappserver/controller/UserControllerTest.java
@@ -1,6 +1,6 @@
-package com.example.demo.controller;
+package com.example.myappserver.controller;
 
-import com.example.myappserver.controller.UserController;
+import com.example.myappserver.MyAppServerApplication;
 import com.example.myappserver.model.User;
 import com.example.myappserver.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
+@ContextConfiguration(classes = MyAppServerApplication.class)
 public class UserControllerTest {
 
     @Autowired
@@ -26,6 +28,7 @@ public class UserControllerTest {
 
     @Test
     public void testCreateUser() throws Exception {
-        System.out.println("hello");
+        // 暂时只是一个空测试
+        System.out.println("Running test");
     }
 } 
